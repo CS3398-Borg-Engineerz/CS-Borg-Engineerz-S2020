daddc9117bff3f1ba6d951e85addceb10ab5db0b
Chance Martin <martin.chancea4@gmail.com>
2020-02-23 20:40:00 -0600

    Create OrgValues.sql
    Add basic dummy inserts for Organizations table

---
+++ OrgsValues.sql
@@ -0,0 +1,14 @@
+INSERT INTO Organization (name, category, dues)
+VALUES ('.EXE', 'pre_graduate', '35')
+
+INSERT INTO Organization (name, category, dues)
+VALUES ('Math Club', 'academic', '25')
+
+INSERT INTO Organization (name, category, dues)
+VALUES ('Writing Club', 'arts', '10')
+
+INSERT INTO Organization (name, category, dues)
+VALUES ('Gym Club', 'fitness', '20')
+
+INSERT INTO Organization (name, category, dues)
+VALUES ('Nursing Club', 'health', '35')
