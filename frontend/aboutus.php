8af41ebb7f43224c82a09eca4de81186e9cd539c
JArmstrong <yak3@txstate.edu>
2020-04-27 09:43:50 -0500

    Adding the instructions for the About us pg

--- frontend/aboutus.php
+++ frontend/aboutus.php
@@ -8,7 +8,7 @@
 
 	<style>
 
-		p{
+		p, ol{
 			font-size:23px;
 		}
 
@@ -27,7 +27,15 @@
 		<p>The purpose of this site is to help Texas State student find organizations and clubs based on their interests. We hope you will use this site to find organizations that spark new and old interests and help you find organizations you want to be a part of. <br><br> Use this site to filter through organizations based on your interests. When you find the club you like get the organization's meeting times, upcoming events and Bio.</p>
 
 		<br><br>
-		<h1>Instructions:</h1>
+		<h1>Instructions:</h1><br>
+		<ol>
+			<li>At the Home page you will find a search bar that allows you to search for the club you are interested in. If you already know the name of the club, type it in and instantly go to the clubs home page to learn about it. If you don't know the club's name then do an empty search to get a list of all the clubs available.</li><br>
+			<li>In the calendar tab you will be able to view all of the events in a calendar format. It helps you schedule your plans based on the clubs you are interested in.</li><br>
+			<li>In the events tab you can view events from all of the clubs to get an idea of what is going on around your school.</li><br>
+			<li>Select the gallery tab to see all of the phots clubs have posted of their events and themselves.</li><br>
+			<li>In settings page you can look at your user profile and edit the data that is saved on your profile.</li><br>
+		</ol>
+		
 		
 
 	</div>
