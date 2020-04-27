392f077fa7904262cb29d44c445a2b68f32a66c1
JArmstrong <yak3@txstate.edu>
2020-04-27 09:27:06 -0500

    creating about us page

---
+++ frontend/aboutus.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title>About Us</title>
+	<?php
+	include("header.php");
+
+?>
+
+	<style>
+
+		p{
+			font-size:23px;
+		}
+
+	</style>
+
+</head>
+
+
+<body>
+	<div style="padding-right:250px;margin-left:50px; margin-top:50px">
+		<h1>About Us</h1>
+
+		<h2>To learn what this site has to offer, read on.</h2><br>
+
+		<p style='font-weight:bold;'>Purpose:</p>
+		<p>The purpose of this site is to help Texas State student find organizations and clubs based on their interests. We hope you will use this site to find organizations that spark new and old interests and help you find organizations you want to be a part of. <br><br> Use this site to filter through organizations based on your interests. When you find the club you like get the organization's meeting times, upcoming events and Bio.</p>
+
+		<br><br>
+		<h1>Instructions:</h1>
+		
+
+	</div>
+</body>
+</html>
