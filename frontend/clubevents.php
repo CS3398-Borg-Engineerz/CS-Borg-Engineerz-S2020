851703edd002357882d77098f221384e99afc76e
JArmstrong <yak3@txstate.edu>
2020-04-27 10:03:39 -0500

    Pulling from database. One event so far

--- frontend/clubevents.php
+++ frontend/clubevents.php
@@ -1,3 +1,49 @@
 <?php
 	include("header.php");
+	include("configSoftEng.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Club Events</title>
+
+        <style>
+
+		.events{
+			font-size:23px;
+		}
+
+	</style>
+
+
+    </head>
+
+    <body>
+    	<div class="events" style="padding-right:250px;margin-left:50px; margin-top:50px">
+    	<h1>Events</h1><br>
+    	<?php
+    		$clubID = 2;
+   		    $data = $conn->query("SELECT * FROM `Events`  WHERE org_id = '$clubID'"); 
+		    if($data->num_rows > 0){
+		        while($row = mysqli_fetch_assoc($data)){
+		            foreach($row as $ind => $val)
+		            {
+		                if($ind != 'org_id'){
+		                    echo "$val";
+		                    if($ind != 'event_day'){
+		                        echo ", ";
+		                    }
+		                }
+		            }
+		        }
+		    }
+    	?>
+
+
+    	</div>
+
+    </body>
+
+
+</html>
