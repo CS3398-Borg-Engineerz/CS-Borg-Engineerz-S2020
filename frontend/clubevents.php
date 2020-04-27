83471b606af6a30daf7c730fa9338e6c20e74ed9
JArmstrong <yak3@txstate.edu>
2020-04-27 10:28:02 -0500

    Finishing CSS touches

--- frontend/clubevents.php
+++ frontend/clubevents.php
@@ -21,7 +21,9 @@
 
     <body>
     	<div class="events" style="padding-right:250px;margin-left:50px; margin-top:50px">
-    	<h1>Events</h1><hr><br>
+    	<h1>Events</h1><hr>
+    	<h2>A list of all the events that are planned for a quick look up.</h2>
+    	<br>
     	<?php
     		$clubID = 2;
     		$clubName = 
@@ -36,7 +38,7 @@
 		                        echo ", ";		                    }
 		                }
 		            }
-                    echo "<br><br>";
+                    echo "<hr>";
 
 		        }
 		    }
