60d4f46e4df1f45c029d3f331bdca9b4c5541f88
Olaedo Nzeogu <55467964+Ola450@users.noreply.github.com>
2020-04-27 23:14:39 -0500

    Update index.php

--- EventCalendar/index.php
+++ EventCalendar/index.php
@@ -4,7 +4,7 @@
 <?php 
 include('header.php');
 ?>
-<title>Calender page</title>
+<title>Calendar Page</title>
 <link rel="stylesheet" href="css/calendar.css">
 <div class="container">	
 	<h2>Event Calendar Page for Organizations</h2>	
@@ -15,6 +15,11 @@ include('header.php');
 				<button class="btn btn-default" data-calendar-nav="today">Today</button>
 				<button class="btn btn-primary" data-calendar-nav="next">Next >></button>
 			</div>
+			<div class="btn-group">
+				<button class="btn btn-warning" data-calendar-view="year">Year</button>
+				<button class="btn btn-warning active" data-calendar-view="month">Month</button>
+				<button class="btn btn-warning" data-calendar-view="week">Week</button>
+			</div>
 		</div>
 		<h3></h3>
 	</div>
@@ -22,8 +27,11 @@ include('header.php');
 		<div class="col-md-9">
 			<div id="showEventCalendar"></div>
 		</div>
+		<div class="col-md-3">
+			<h4>All Events List</h4>
+			<ul id="eventlist" class="nav nav-list"></ul>
+		</div>
 	</div>	
-
 </div>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
 <script type="text/javascript" src="js/calendar.js"></script>
